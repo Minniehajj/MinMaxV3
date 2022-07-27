8121fdcfad51a40c8ee2b74732f9269b39495d9d
Minhajul Hoq <mhoq@mindstreaminteractive.com>
2022-07-27 18:40:33 -0400

    updated card fetching

--- src/components/atoms/CardToolTip/CardToolTip.tsx
+++ src/components/atoms/CardToolTip/CardToolTip.tsx
@@ -9,30 +9,27 @@ import { trpc } from "utils/trpc";
 let CardToolTip: FC<CardToolTipProps> = ({ amount, name, ...props }) => {
   const [image, setImage] = useState<any>("");
   const [tooltipOpen, setTooltipOpen] = useState<boolean>(false);
-  const { data, refetch, isLoading } = trpc.useQuery([
-    "card.getCard",
+  const { data, refetch, isLoading } = trpc.useQuery(
+    [
+      "card.getCard",
+      {
+        card: name,
+      },
+    ],
     {
-      card: name,
-    },
-  ]);
-  // if (tooltipOpen && !isLoading) {
-  //   if (data?.image_uris?.png) {
-  //     console.log(data.image_uris.png);
-  //     // setImage(data?.image_uris?.png);
-  //   } else if (data?.card_faces) {
-  //     // setImage(data?.card_faces[0]?.image_uris?.png);
-  //   } else {
-  //     // setImage("");
-  //   }
-  // }
-
+      enabled: !!tooltipOpen,
+    }
+  );
   return (
     <Tooltip.Provider delayDuration={0}>
       <Tooltip.Root open={tooltipOpen} onOpenChange={(open) => setTooltipOpen(open)}>
         <Tooltip.Trigger asChild>
           <button
             className={`${props.className} text-theme-blue-dark dark:text-theme-blue`}
-            onClick={() => setTooltipOpen(!tooltipOpen)}
+            onClick={() => {
+              refetch();
+              setTooltipOpen(!tooltipOpen);
+            }}
           >
             {amount && <span>{amount} </span>}
             <span>{name}</span>
@@ -40,7 +37,9 @@ let CardToolTip: FC<CardToolTipProps> = ({ amount, name, ...props }) => {
         </Tooltip.Trigger>
         <Tooltip.Content className="rounded-sm">
           {data?.image_uris && <Image src={data.image_uris.png} alt={data.name} width={248} height={346} />}
-          {/* <div>{image && <Image src={image} width={248} height={346} alt="test" />}</div> */}
+          {data?.card_faces && (
+            <Image src={data.card_faces[0]?.image_uris?.png || ""} alt={data.name} width={248} height={346} />
+          )}
           <Tooltip.Arrow />
         </Tooltip.Content>
       </Tooltip.Root>
